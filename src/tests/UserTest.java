5112a05118280166876712295e7bc90597a2bcc6
Maskady <florent.tout@gmail.com>
2024-11-12 15:16:05 +0100

    Modify tests to be more efficient

--- src/tests/UserTest.java
+++ src/tests/UserTest.java
@@ -1,9 +1,16 @@
 package tests;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import location.PersonalInformation;
 import location.User;
+import location.Reservation;
+import location.RentingException;
+import location.Movie;
+import java.util.Date;
 
 public class UserTest {
 
@@ -11,11 +18,21 @@ public class UserTest {
   private static final String PASSWORD = "password123";
   private static final String LASTNAME_JOHN = "John";
   private static final String FIRSTNAME_DOE = "Doe";
+  private User user;
+  private PersonalInformation info;
+  private Movie movie;
+
+  @BeforeEach
+  public void setUp() {
+    info = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
+    user = User.register(USERNAME_JOHNDOE, PASSWORD, info);
+    user.resetReservations();
+    User.resetUsers();
+    movie = new Movie("Interstellar", 2024, null, null);
+  }
 
   @Test
   public void testRegisterSuccess() {
-    PersonalInformation info = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
-    User user = User.register(USERNAME_JOHNDOE, PASSWORD, info);
     assertNotNull(user);
     assertEquals(USERNAME_JOHNDOE, user.getLogin());
     assertEquals(info, user.getPersonalInformation());
@@ -26,56 +43,48 @@ public class UserTest {
     PersonalInformation info1 = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
     User.register(USERNAME_JOHNDOE, PASSWORD, info1);
     PersonalInformation info2 = new PersonalInformation("Jane", FIRSTNAME_DOE);
-    User user = User.register(USERNAME_JOHNDOE, "password456", info2);
+    user = User.register(USERNAME_JOHNDOE, "password456", info2);
     assertNull(user);
   }
 
   @Test
   public void testRegisterEmptyUsername() {
-    PersonalInformation info = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
-    User user = User.register("", PASSWORD, info);
+    user = User.register("", PASSWORD, info);
     assertNull(user);
   }
 
   @Test
   public void testRegisterEmptyPassword() {
-    PersonalInformation info = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
-    User user = User.register(USERNAME_JOHNDOE, "", info);
+    user = User.register(USERNAME_JOHNDOE, "", info);
     assertNull(user);
   }
 
   @Test
   public void testRegisterNullUsername() {
-    PersonalInformation info = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
-    User user = User.register(null, PASSWORD, info);
+    user = User.register(null, PASSWORD, info);
     assertNull(user);
   }
 
   @Test
   public void testRegisterNullPassword() {
-    PersonalInformation info = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
-    User user = User.register(USERNAME_JOHNDOE, null, info);
+    user = User.register(USERNAME_JOHNDOE, null, info);
     assertNull(user);
   }
 
   @Test
   public void testRegisterNullPersonalInformation() {
-    User user = User.register(USERNAME_JOHNDOE, PASSWORD, null);
+    user = User.register(USERNAME_JOHNDOE, PASSWORD, null);
     assertNull(user);
   }
 
   @Test
   public void testGetLogin() {
-    PersonalInformation info = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
-    User user = User.register(USERNAME_JOHNDOE, PASSWORD, info);
     assertNotNull(user);
     assertEquals(USERNAME_JOHNDOE, user.getLogin());
   }
 
   @Test
   public void testSetLogin() {
-    PersonalInformation info = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
-    User user = User.register(USERNAME_JOHNDOE, PASSWORD, info);
     assertNotNull(user);
     user.setLogin("newlogin");
     assertEquals("newlogin", user.getLogin());
@@ -83,16 +92,12 @@ public class UserTest {
 
   @Test
   public void testGetPassword() {
-    PersonalInformation info = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
-    User user = User.register(USERNAME_JOHNDOE, PASSWORD, info);
     assertNotNull(user);
     assertEquals(PASSWORD, user.getPassword());
   }
 
   @Test
   public void testSetPassword() {
-    PersonalInformation info = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
-    User user = User.register(USERNAME_JOHNDOE, PASSWORD, info);
     assertNotNull(user);
     user.setPassword("newpassword");
     assertEquals("newpassword", user.getPassword());
@@ -100,16 +105,12 @@ public class UserTest {
 
   @Test
   public void testGetPersonalInformation() {
-    PersonalInformation info = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
-    User user = User.register(USERNAME_JOHNDOE, PASSWORD, info);
     assertNotNull(user);
     assertEquals(info, user.getPersonalInformation());
   }
 
   @Test
   public void testSetPersonalInformation() {
-    PersonalInformation info = new PersonalInformation(LASTNAME_JOHN, FIRSTNAME_DOE);
-    User user = User.register(USERNAME_JOHNDOE, PASSWORD, info);
     assertNotNull(user);
     PersonalInformation newInfo = new PersonalInformation("Jane", FIRSTNAME_DOE);
     user.setPersonalInformation(newInfo);
