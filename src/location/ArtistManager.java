12211f3fbdd0d5ded8019acc634dde1818a35f49
Paul Hariel <paul.hariel@outlook.com>
2024-12-03 14:49:16 +0100

    Add two sets for actors and directors

--- src/location/ArtistManager.java
+++ src/location/ArtistManager.java
@@ -11,20 +11,27 @@ public class ArtistManager {
   /**
    * Represents all the artists.
    */
-  private Set<Artist> artists ;
+  private Set<Artist> actors = new HashSet<>();
+  
+  /**
+   * Represents all the directors.
+   */
+  private Set<Artist> directors = new HashSet<>();
 
   /**
    * Constructs a new ArtistManager.
    */
   public ArtistManager() {
-    this.artists = new HashSet<>();
   }
 
   /** 
    * @return the artists.
    */
   public Set<Artist> getArtists() {
-    return this.artists;
+    Set<Artist> result = new HashSet<Artist>();
+    result.addAll(this.actors);
+    result.addAll(this.directors);
+    return result;
   }
 
   /**
@@ -36,7 +43,7 @@ public class ArtistManager {
   public Set<Artist> getArtists(Predicate<Artist> p) {
     Set<Artist> result = new HashSet<>();
 
-    for (Artist artist : this.artists) {
+    for (Artist artist : this.getArtists()) {
       if (p.test(artist)) {
         result.add(artist);
       }
@@ -44,5 +51,24 @@ public class ArtistManager {
 
     return result;
   }
-    
+
+  public Set<Artist> getActors() {
+    return this.actors;
+  }
+
+  public Set<Artist> getDirectors() {
+    return this.directors;
+  }
+
+  public boolean deleteArtist(Artist artist) {
+    return this.actors.remove(artist) || this.directors.remove(artist);
+  }
+
+  public void addActor(Artist artist) {
+    this.actors.add(artist);
+  }
+
+  public void addDirector(Artist artist) {
+    this.directors.add(artist);
+  }
 }
